5ff67c1875908ecbc0f6fdf310712c8cb47b1d28
Kelsey <17769981+kmorsebrown@users.noreply.github.com>
2021-12-21 20:19:05 -0600

    Day 6 part 2 solve

--- Day06/day6_part2.js
+++ Day06/day6_part2.js
@@ -2,7 +2,9 @@
 const {
     getFileContents,
     convertToNumArray,
-    incrementDays
+    countOccurances,
+    getNumFishAtAge,
+    getNumFishAfterGivenDay
 } = require('./functions');
 
 //Get data and convert it
@@ -13,3 +15,12 @@ const testFishArr = convertToNumArray(testInput);
 const puzzleFishArr = convertToNumArray(puzzleInput)
 
 //Part 2
+const d = 256;
+function getNumFishAtGivenDay(fishArr,numDays) {
+    const startingFishAgeArr = getNumFishAtAge(fishArr);
+    const endFishAgeArr = getNumFishAfterGivenDay(startingFishAgeArr,numDays);
+    return endFishAgeArr.reduce((a, b) => a + b, 0);
+}
+
+console.log('Testing Num Fish After',d,'days:',getNumFishAtGivenDay(testFishArr,d));
+console.log('Puzzle Num Fish After',d,'days:',getNumFishAtGivenDay(puzzleFishArr,d));
--- Day06/functions.js
+++ Day06/functions.js
@@ -42,8 +42,48 @@ function incrementDays(arr, numDays) {
     return workingArr;
 }
 
+const countOccurances = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+const getNumFishAtAge = (arr) => (v = [
+    countOccurances(arr,0),
+    countOccurances(arr,1),
+    countOccurances(arr,2),
+    countOccurances(arr,3),
+    countOccurances(arr,4),
+    countOccurances(arr,5),
+    countOccurances(arr,6),
+    countOccurances(arr,7),
+    countOccurances(arr,8)
+]);
+
+
+function getNumFishAfterGivenDay (arr,days) {
+  let workingArr = [...arr];
+
+  for (i = 1; i <= days; i++) {
+    let tempArr = [
+      workingArr[1],
+      workingArr[2],
+      workingArr[3],
+      workingArr[4],
+      workingArr[5],
+      workingArr[6],
+      workingArr[7] + workingArr[0],
+      workingArr[8],
+      workingArr[0]
+    ];
+
+    workingArr = tempArr;
+  }
+
+  return workingArr;
+}
+
 module.exports = {
     getFileContents,
     convertToNumArray,
-    incrementDays
+    incrementDays,
+    countOccurances,
+    getNumFishAtAge,
+    getNumFishAfterGivenDay
 }
