0587c6b60fd047c69562cfaeb0178a397572e5cb
Kelsey <17769981+kmorsebrown@users.noreply.github.com>
2021-12-21 18:57:22 -0600

    Day 6 part one answer

--- Day06/day6_part1.js
+++ Day06/day6_part1.js
@@ -1,15 +1,18 @@
 //Import functions
 const {
     getFileContents,
-    convertToNumArray
+    convertToNumArray,
+    incrementDays
 } = require('./functions');
 
 //Get data and convert it
 const testInput = getFileContents("/testInput.txt");
 const puzzleInput = getFileContents("/puzzleInput.txt");
 
-const listOfLanternfish = convertToNumArray(testInput);
-
-//console.log(listOfLanternfish);
+const testFishArr = convertToNumArray(testInput);
+const puzzleFishArr = convertToNumArray(puzzleInput)
 
 //Start on Part 1
+console.log('Num fish after 80 days')
+console.log('Test Data',incrementDays(testFishArr,80).length);
+console.log('Puzzle Data',incrementDays(puzzleFishArr,80).length);
--- Day06/functions.js
+++ Day06/functions.js
@@ -20,7 +20,30 @@ function convertToNumArray(input) {
     return newArr;
 }
 
+function incrementDays(arr, numDays) {
+    let workingArr = [...arr];
+
+    for (day = 1; day <= numDays; day++) {
+        let tempArr = [...workingArr];
+
+        for (i = 0; i < workingArr.length; i++) {
+
+            if (tempArr[i] > 0) {
+                tempArr[i] -= 1
+            } else {
+                tempArr.push(8);
+                tempArr[i] = 6;
+            }
+        }
+      workingArr = tempArr;
+      //console.log('After',day,'days:',workingArr);
+    }
+
+    return workingArr;
+}
+
 module.exports = {
     getFileContents,
-    convertToNumArray
+    convertToNumArray,
+    incrementDays
 }
