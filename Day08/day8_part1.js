5d9bf051073b338688a8d407782bf93af8c84e2f
Kelsey <17769981+kmorsebrown@users.noreply.github.com>
2021-12-22 17:00:03 -0600

    Day 8 part 1 solve

--- Day08/day8_part1.js
+++ Day08/day8_part1.js
@@ -1,21 +1,45 @@
 //Import functions
 const {
     getFileContents,
+    convertTxtToJsObj,
+    countInstacesOfOuputLength
 } = require('./functions');
 
 //Get data and convert it
-const testInput = getFileContents("/testInput.txt");
-const puzzleInput = getFileContents("/puzzleInput.txt");
+const testInput = convertTxtToJsObj(getFileContents("/testInput.txt"));
+const puzzleInput = convertTxtToJsObj(getFileContents("/puzzleInput.txt"))
 
 const key = {
-    'zero': [a,b,c,e,f,g], //6 letters
-    'one': [c,f], //2 letters
-    'two': [a,c,d,e,g], //5 letters
-    'three': [a,c,d,f,g], //5 letters
-    'four': [b,c,d,f], //4 letters
-    'five': [a,b,d,f,g], //5 letters
-    'six': [a,b,d,e,f,g], //6 letters
-    'seven': [a,c,f], //3 letters
-    'eight': [a,b,c,d,e,f,g], //7 letters
-    'nine': [a,b,c,d,f,g] //6 letters
+    'zero': ['a','b','c','e','f','g'], //6 letters
+    'one': ['c','f'], //2 letters
+    'two': ['a','c','d','e','g'], //5 letters
+    'three': ['a','c','d','f','g'], //5 letters
+    'four': ['b','c','d','f'], //4 letters
+    'five': ['a','b','d','f','g'], //5 letters
+    'six': ['a','b','d','e','f','g'], //6 letters
+    'seven': ['a','c','f'], //3 letters
+    'eight': ['a','b','c','d','e','f','g'], //7 letters
+    'nine': ['a','b','c','d','f','g'] //6 letters
 }
+
+const testOutputLengthCounts = countInstacesOfOuputLength(testInput);
+const puzzleOutputLengthCounts = countInstacesOfOuputLength(puzzleInput);
+
+
+//In the output values, how many times do digits 1, 4, 7, or 8 appear?
+
+function getPartOneSolution (obj) {
+    let answer =
+        //Digit 1: 2 characters
+        obj.two +
+        //Digit 4: 4 characters
+        obj.four +
+        //Digit 7: 3 characters
+        obj.three +
+        //Digit 8: 7 characters
+        obj.seven;
+    return answer;
+};
+
+console.log ('Testing Answer:',getPartOneSolution(testOutputLengthCounts));
+console.log ('Puzzle Answer:',getPartOneSolution(puzzleOutputLengthCounts));
--- Day08/functions.js
+++ Day08/functions.js
@@ -9,6 +9,66 @@ function getFileContents(filename) {
     return buffer.toString();
 }
 
+function convertTxtToJsObj(input) {
+    const arr = input.split(/\r?\n/);
+    var jsonObj = [];
+    var headers = ['signal', 'output'];
+  
+    for(var i = 0; i < arr.length; i++) {
+      var data = arr[i].split('|');
+      var obj = {};
+      for(var j = 0; j < data.length; j++) {
+         obj[headers[j].trim()] = data[j].trim();
+      }
+      jsonObj.push(obj);
+    }
+    
+    for (var i = 0; i < jsonObj.length; i++) {
+      jsonObj[i].signal = jsonObj[i].signal.split(' ');
+      jsonObj[i].output = jsonObj[i].output.split(' ');
+    }
+    return jsonObj;
+}
+
+function countInstacesOfOuputLength (arrOfObj,num) {
+    let numInstances = {
+        'two': 0,
+        'three': 0,
+        'four': 0,
+        'five': 0,
+        'six': 0,
+        'seven': 0
+      };
+  
+      for (i = 0; i < arrOfObj.length; i++) {
+          for (j = 0; j < arrOfObj[i].output.length; j++) {
+            switch (arrOfObj[i].output[j].length) {
+              case 2:
+                numInstances.two += 1
+                break;
+              case 3:
+                numInstances.three += 1
+                break;
+              case 4:
+                numInstances.four += 1
+                break;
+              case 5:
+                numInstances.five += 1
+                break;
+              case 6:
+                numInstances.six += 1
+                break;
+              case 7:
+                numInstances.seven += 1
+                break;
+            }
+          }
+      }
+    return numInstances;
+}
+
 module.exports = {
-    getFileContents
+    getFileContents,
+    convertTxtToJsObj,
+    countInstacesOfOuputLength
 };
